d587338dd35ae380cf9f71854ef169f76ff53403
anisfarehan <anis_farehan93@hotmail.com>
2017-04-12 23:31:49 +0800

    Wednesday 12-Apr-17 11:31:47 PM

--- src/com/ayerputeh/GlobalServlet/LoginServlet.java
+++ src/com/ayerputeh/GlobalServlet/LoginServlet.java
@@ -17,30 +17,34 @@ import java.io.IOException;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        /*String no_ic = request.getParameter("number_ic");
-        String password = request.getParameter("password");*/
-
-        LoginDAO loginDAO = new LoginDAO().read(1);
-
-        /*
-        * convert object to JSON
-        * */
-        Gson gson = new Gson();
-        String json = gson.toJson(loginDAO);
-
-
-
         JsonObject data = new Gson().fromJson(request.getReader(), JsonObject.class);
         String number_ic = data.get("number_ic").getAsString();
         String password = data.get("password").getAsString();
 
+        LoginDAO loginDAO = new LoginDAO().findByNRIC(number_ic);
+
+        if (loginDAO.password == password) {
+        /*
+        * convert object to JSON
+        * */
+            Gson gson = new Gson();
+            String json = gson.toJson(loginDAO);
+
+
         /*
         * response the JSON
         * */
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().print(json);
+            response.getWriter().flush();
+        }
+
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().print(password);
+        response.getWriter().print("");
         response.getWriter().flush();
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
