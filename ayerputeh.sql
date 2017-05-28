5047d0d1868a5e829f229a018607866a29e99b75
Mizan <mexists@yahoo.com>
2017-05-28 18:35:44 +0800

    Sunday 28-May-17  6:35:44

--- ayerputeh.sql
+++ ayerputeh.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 28, 2017 at 06:58 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Host: localhost
+-- Generation Time: May 28, 2017 at 10:35 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.2
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ayerputeh`
@@ -26,13 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `aktiviti`
 --
 
-CREATE TABLE IF NOT EXISTS `aktiviti` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `aktiviti` (
+  `id` int(12) NOT NULL,
   `aktivitinama` varchar(200) NOT NULL,
   `tarikh` varchar(40) NOT NULL,
-  `huraian` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+  `huraian` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `aktiviti`
@@ -59,12 +58,11 @@ INSERT INTO `aktiviti` (`id`, `aktivitinama`, `tarikh`, `huraian`) VALUES
 -- Table structure for table `final`
 --
 
-CREATE TABLE IF NOT EXISTS `final` (
-  `id` int(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `final` (
+  `id` int(20) NOT NULL,
   `subjek_id` int(20) NOT NULL,
-  `jumlah` decimal(50,0) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `jumlah` decimal(50,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -72,15 +70,13 @@ CREATE TABLE IF NOT EXISTS `final` (
 -- Table structure for table `guru`
 --
 
-CREATE TABLE IF NOT EXISTS `guru` (
-  `id` int(200) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `guru` (
+  `id` int(200) NOT NULL,
   `gurunama` varchar(100) NOT NULL,
   `no_ic` varchar(20) NOT NULL,
   `subjek_id` varchar(200) NOT NULL,
-  `kelas_id` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+  `kelas_id` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `guru`
@@ -126,7 +122,7 @@ INSERT INTO `guru` (`id`, `gurunama`, `no_ic`, `subjek_id`, `kelas_id`) VALUES
 -- Table structure for table `guru_subjek`
 --
 
-CREATE TABLE IF NOT EXISTS `guru_subjek` (
+CREATE TABLE `guru_subjek` (
   `guru_id` int(11) DEFAULT NULL,
   `subjek_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -147,12 +143,11 @@ INSERT INTO `guru_subjek` (`guru_id`, `subjek_id`) VALUES
 -- Table structure for table `kelas`
 --
 
-CREATE TABLE IF NOT EXISTS `kelas` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `kelas` (
+  `id` int(12) NOT NULL,
   `kelasnama` varchar(20) NOT NULL,
-  `guru_id` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+  `guru_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kelas`
@@ -187,73 +182,73 @@ INSERT INTO `kelas` (`id`, `kelasnama`, `guru_id`) VALUES
 -- Table structure for table `login`
 --
 
-CREATE TABLE IF NOT EXISTS `login` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
   `number_ic` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `gcm_registration_id` text COMMENT 'Device token',
   `status` varchar(255) DEFAULT NULL,
   `nama` varchar(200) NOT NULL,
   `guru` varchar(200) NOT NULL,
-  `penyelia` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=61 ;
+  `penyelia` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `number_ic`, `password`, `status`, `nama`, `guru`, `penyelia`) VALUES
-(1, '920319035066', 'fakihi', 'Penyelia', 'Farah Wahida Binti Muhammad Fakihi', '', 'penyelia'),
-(2, '950529035333', '950529035333', 'pelajar', 'Muhammad Muaz Hafifi Bin Muhammad Fakihi', '', ''),
-(12, '0', 'syayasya', 'Guru', 'ooooo', 'guru', ''),
-(13, '90000', '90000', 'Guru', 'Razak', 'guru', ''),
-(14, '930317035924', '930317035924', 'Pelajar', 'Anis Farehan Binti Muhammad Fakihi', '', ''),
-(15, '987654321', '12345', 'Pelajar', 'alon', '', ''),
-(16, '9876543211', '9876543211', 'Pelajar', 'alon', '', ''),
-(17, '1234567890', '1234567890', 'Pelajar', 'qwertyuiop', '', ''),
-(18, '9000', '9000', 'Guru', 'farehan', 'guru', ''),
-(19, 'bhjbh', 'awak', 'Guru', 'huhuu', 'guru', ''),
-(20, '19999', '19999', 'pelajar', 'lengkap', '', ''),
-(21, '91000', '91000', 'pelajar', 'anis', '', ''),
-(22, '92022', '92022', 'Guru', 'siti', 'guru', ''),
-(23, '99333', '99333', 'pelajar', 'farehaun', '', ''),
-(24, '961211111', '961211111', 'pelajar', 'Wan Nur Aqmar Syaira', '', ''),
-(25, '60080903500', '60080903500', 'Guru', 'muhammad fakihi', 'guru', ''),
-(26, '', '', 'Guru', '', 'guru', ''),
-(27, '', '', 'Guru', '', 'guru', ''),
-(28, '', '', 'Guru', '', 'guru', ''),
-(29, '', '', 'Guru', '', 'guru', ''),
-(30, '', '', 'Guru', '', 'guru', ''),
-(31, '234567890', '234567890', 'pelajar', 'qwertyuiolmncgh', '', ''),
-(32, '930303', '930303', 'Guru', 'sayuti', 'guru', ''),
-(33, '9000888', '9000888', 'Guru', 'ahmad amril', 'guru', ''),
-(34, '0233342211', '0233342211', 'Guru', 'muhammad zakaria', 'guru', ''),
-(35, '8900098', '8900098', 'Guru', 'zain zaidin', 'guru', ''),
-(36, '8900098', '8900098', 'Guru', 'zain zaidin', 'guru', ''),
-(37, '95067780', '95067780', 'Guru', 'syasya sazali', 'guru', ''),
-(38, '9999', '9999', 'Guru', 'amira', 'guru', ''),
-(39, '9999', '9999', 'Guru', 'amira', 'guru', ''),
-(40, '9999', '9999', 'Guru', 'amira', 'guru', ''),
-(41, '00990', '00990', 'Guru', 'aqilah', 'guru', ''),
-(42, '1234567890', '1234567890', 'Guru', 'aq', 'guru', ''),
-(43, '877777777777', '877777777777', 'Guru', 'alisas', 'guru', ''),
-(44, '99567766', '99567766', 'Guru', 'muazzz', 'guru', ''),
-(45, '000', '000', 'Guru', 'yhis ijii', 'guru', ''),
-(46, '870009', '870009', 'Guru', 'amri yahya', 'guru', ''),
-(47, '123456789', '123456789', 'Guru', 'qwertyuioinjbhgfvcd1234567', 'guru', ''),
-(48, '90', '90', 'Guru', 'zalali', 'guru', ''),
-(49, '900', '900', 'Guru', 'melakasana', 'guru', ''),
-(50, '93042806499', '93042806499', 'Guru', 'fadhilah', 'guru', ''),
-(51, '60999999', '60999999', 'pelajar', 'siti aminah ', '', ''),
-(52, '09999', '09999', 'pelajar', 'zul yahya', '', ''),
-(53, '92044443', '92044443', 'pelajar', 'Zaidatul ain', '', ''),
-(54, '090005677', '090005677', 'pelajar', 'NUR ZULI BINTI AMRI', '', ''),
-(55, '010456065667', '010456065667', 'pelajar', 'aqwa hazri', '', ''),
-(56, '010456065667', '010456065667', 'pelajar', 'aqwa hazri', '', ''),
-(57, '010456065667', '010456065667', 'pelajar', 'aqwa hazri', '', ''),
-(58, '678999900', '678999900', 'Guru', 'zainal abidin', 'guru', ''),
-(59, '090', '090', 'pelajar', 'muhammad muaz hafifi', '', ''),
-(60, '9000', '9000', 'Guru', 'zainuri', 'guru', '');
+INSERT INTO `login` (`id`, `number_ic`, `password`, `gcm_registration_id`, `status`, `nama`, `guru`, `penyelia`) VALUES
+(1, '920319035066', 'fakihi', NULL, 'Penyelia', 'Farah Wahida Binti Muhammad Fakihi', '', 'penyelia'),
+(2, '950529035333', '950529035333', NULL, 'pelajar', 'Muhammad Muaz Hafifi Bin Muhammad Fakihi', '', ''),
+(12, '0', 'syayasya', NULL, 'Guru', 'ooooo', 'guru', ''),
+(13, '90000', '90000', NULL, 'Guru', 'Razak', 'guru', ''),
+(14, '930317035924', '930317035924', 'daA5GYJIXpU:APA91bGDybtoj6BwWlABDu5P7bQzKKaFfFIFGioHWXDYokAqHDjuxA2W2xyimnPT09ANa9j4fN5PgW4nUdkF2cu-xKrS5YtJKkbP2uiaRb02oPoZ4Dx53OIoV51OMb21fn81QTVnjR11', 'Pelajar', 'Anis Farehan Binti Muhammad Fakihi', '', ''),
+(15, '987654321', '12345', NULL, 'Pelajar', 'alon', '', ''),
+(16, '9876543211', '9876543211', NULL, 'Pelajar', 'alon', '', ''),
+(17, '1234567890', '1234567890', NULL, 'Pelajar', 'qwertyuiop', '', ''),
+(18, '9000', '9000', NULL, 'Guru', 'farehan', 'guru', ''),
+(19, 'bhjbh', 'awak', NULL, 'Guru', 'huhuu', 'guru', ''),
+(20, '19999', '19999', NULL, 'pelajar', 'lengkap', '', ''),
+(21, '91000', '91000', NULL, 'pelajar', 'anis', '', ''),
+(22, '92022', '92022', NULL, 'Guru', 'siti', 'guru', ''),
+(23, '99333', '99333', NULL, 'pelajar', 'farehaun', '', ''),
+(24, '961211111', '961211111', NULL, 'pelajar', 'Wan Nur Aqmar Syaira', '', ''),
+(25, '60080903500', '60080903500', NULL, 'Guru', 'muhammad fakihi', 'guru', ''),
+(26, '', '', NULL, 'Guru', '', 'guru', ''),
+(27, '', '', NULL, 'Guru', '', 'guru', ''),
+(28, '', '', NULL, 'Guru', '', 'guru', ''),
+(29, '', '', NULL, 'Guru', '', 'guru', ''),
+(30, '', '', NULL, 'Guru', '', 'guru', ''),
+(31, '234567890', '234567890', NULL, 'pelajar', 'qwertyuiolmncgh', '', ''),
+(32, '930303', '930303', NULL, 'Guru', 'sayuti', 'guru', ''),
+(33, '9000888', '9000888', NULL, 'Guru', 'ahmad amril', 'guru', ''),
+(34, '0233342211', '0233342211', NULL, 'Guru', 'muhammad zakaria', 'guru', ''),
+(35, '8900098', '8900098', NULL, 'Guru', 'zain zaidin', 'guru', ''),
+(36, '8900098', '8900098', NULL, 'Guru', 'zain zaidin', 'guru', ''),
+(37, '95067780', '95067780', NULL, 'Guru', 'syasya sazali', 'guru', ''),
+(38, '9999', '9999', NULL, 'Guru', 'amira', 'guru', ''),
+(39, '9999', '9999', NULL, 'Guru', 'amira', 'guru', ''),
+(40, '9999', '9999', NULL, 'Guru', 'amira', 'guru', ''),
+(41, '00990', '00990', NULL, 'Guru', 'aqilah', 'guru', ''),
+(42, '1234567890', '1234567890', NULL, 'Guru', 'aq', 'guru', ''),
+(43, '877777777777', '877777777777', NULL, 'Guru', 'alisas', 'guru', ''),
+(44, '99567766', '99567766', NULL, 'Guru', 'muazzz', 'guru', ''),
+(45, '000', '000', NULL, 'Guru', 'yhis ijii', 'guru', ''),
+(46, '870009', '870009', NULL, 'Guru', 'amri yahya', 'guru', ''),
+(47, '123456789', '123456789', NULL, 'Guru', 'qwertyuioinjbhgfvcd1234567', 'guru', ''),
+(48, '90', '90', NULL, 'Guru', 'zalali', 'guru', ''),
+(49, '900', '900', NULL, 'Guru', 'melakasana', 'guru', ''),
+(50, '93042806499', '93042806499', NULL, 'Guru', 'fadhilah', 'guru', ''),
+(51, '60999999', '60999999', NULL, 'pelajar', 'siti aminah ', '', ''),
+(52, '09999', '09999', NULL, 'pelajar', 'zul yahya', '', ''),
+(53, '92044443', '92044443', NULL, 'pelajar', 'Zaidatul ain', '', ''),
+(54, '090005677', '090005677', NULL, 'pelajar', 'NUR ZULI BINTI AMRI', '', ''),
+(55, '010456065667', '010456065667', NULL, 'pelajar', 'aqwa hazri', '', ''),
+(56, '010456065667', '010456065667', NULL, 'pelajar', 'aqwa hazri', '', ''),
+(57, '010456065667', '010456065667', NULL, 'pelajar', 'aqwa hazri', '', ''),
+(58, '678999900', '678999900', NULL, 'Guru', 'zainal abidin', 'guru', ''),
+(59, '090', '090', NULL, 'pelajar', 'muhammad muaz hafifi', '', ''),
+(60, '9000', '9000', NULL, 'Guru', 'zainuri', 'guru', '');
 
 -- --------------------------------------------------------
 
@@ -261,8 +256,8 @@ INSERT INTO `login` (`id`, `number_ic`, `password`, `status`, `nama`, `guru`, `p
 -- Table structure for table `pelajar`
 --
 
-CREATE TABLE IF NOT EXISTS `pelajar` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pelajar` (
+  `id` int(11) NOT NULL,
   `pelajarnama` varchar(200) NOT NULL,
   `no_ic` varchar(12) NOT NULL,
   `namaibu` varchar(200) NOT NULL,
@@ -272,10 +267,8 @@ CREATE TABLE IF NOT EXISTS `pelajar` (
   `kelas_id` varchar(20) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `poskod` varchar(10) NOT NULL,
-  `negeri` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+  `negeri` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pelajar`
@@ -312,12 +305,11 @@ INSERT INTO `pelajar` (`id`, `pelajarnama`, `no_ic`, `namaibu`, `namabapa`, `aga
 -- Table structure for table `penggal1`
 --
 
-CREATE TABLE IF NOT EXISTS `penggal1` (
+CREATE TABLE `penggal1` (
   `id` int(20) NOT NULL,
   `subjek_id` varchar(20) NOT NULL,
   `jumlah1` varchar(200) NOT NULL,
-  `no_ic` varchar(14) NOT NULL,
-  PRIMARY KEY (`id`)
+  `no_ic` varchar(14) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -344,13 +336,12 @@ INSERT INTO `penggal1` (`id`, `subjek_id`, `jumlah1`, `no_ic`) VALUES
 -- Table structure for table `penggal2`
 --
 
-CREATE TABLE IF NOT EXISTS `penggal2` (
-  `id` int(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `penggal2` (
+  `id` int(20) NOT NULL,
   `subjek_id` varchar(20) NOT NULL,
   `jumlah2` varchar(100) NOT NULL,
-  `no_ic` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `no_ic` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -358,11 +349,10 @@ CREATE TABLE IF NOT EXISTS `penggal2` (
 -- Table structure for table `subjek`
 --
 
-CREATE TABLE IF NOT EXISTS `subjek` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
-  `subjeknama` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+CREATE TABLE `subjek` (
+  `id` int(12) NOT NULL,
+  `subjeknama` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subjek`
@@ -375,6 +365,110 @@ INSERT INTO `subjek` (`id`, `subjeknama`) VALUES
 (4, 'Inggeris'),
 (5, 'Bahasa Melayu 2');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aktiviti`
+--
+ALTER TABLE `aktiviti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `final`
+--
+ALTER TABLE `final`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `guru`
+--
+ALTER TABLE `guru`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pelajar`
+--
+ALTER TABLE `pelajar`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `penggal1`
+--
+ALTER TABLE `penggal1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penggal2`
+--
+ALTER TABLE `penggal2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjek`
+--
+ALTER TABLE `subjek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aktiviti`
+--
+ALTER TABLE `aktiviti`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `final`
+--
+ALTER TABLE `final`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `guru`
+--
+ALTER TABLE `guru`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+--
+-- AUTO_INCREMENT for table `pelajar`
+--
+ALTER TABLE `pelajar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `penggal2`
+--
+ALTER TABLE `penggal2`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `subjek`
+--
+ALTER TABLE `subjek`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
